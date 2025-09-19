ad785faace46845f323d0ecd3eda0520722fe23c
Programmer70259 <156535882+Programmer3152@users.noreply.github.com>
2025-09-20 00:09:04 +0530

    Update Q1_ADD.cpp

--- C++ DSA UCS301/Assignment_4/Assignment_4_Additional/Q1_ADD.cpp
+++ C++ DSA UCS301/Assignment_4/Assignment_4_Additional/Q1_ADD.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+#include <queue>
 
+using namespace std;
+
+void genBin(int n)
+{
+    queue<string> q;
+    q.push("1");
+    for(int i = 0; i < n; i++)
+    {
+        string curr = q.front();
+        cout << curr << " ";
+        q.pop();
+        q.push(curr + "0");
+        q.push(curr + "1");
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n = 5;
+    genBin(n);
+    return 0;
+}
