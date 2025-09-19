5896c9c944b39efa5de84d2575ba1cac0db9b49f
Programmer70259 <156535882+Programmer3152@users.noreply.github.com>
2025-09-20 00:08:26 +0530

    Update Q1_ADD.cpp

--- C++ DSA UCS301/Assignment_5/Assignment_5_Additional/Q1_ADD.cpp
+++ C++ DSA UCS301/Assignment_5/Assignment_5_Additional/Q1_ADD.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 
+using namespace std;
+
+struct Node { int data; Node* next; };
+
+Node* getIntrsctn(Node* h1, Node* h2)
+{
+    Node *p1 = h1, *p2 = h2;
+    while (p1 != p2)
+    {
+        p1 = (p1 == nullptr) ? h2 : p1->next;
+        p2 = (p2 == nullptr) ? h1 : p2->next;
+    }
+    return p1;
+}
+
+int main()
+{
+    Node* newNode;
+    Node* h1 = new Node(); h1->data = 4;
+    Node* h2 = new Node(); h2->data = 5;
+    newNode = new Node(); newNode->data = 1; h1->next = newNode;
+    newNode = new Node(); newNode->data = 6; h2->next = newNode;
+    newNode = new Node(); newNode->data = 1; h2->next->next = newNode;
+    Node* intersect = new Node(); intersect->data = 8;
+    h1->next->next = intersect; h2->next->next->next = intersect;
+    newNode = new Node(); newNode->data = 5; intersect->next = newNode;
+    
+    Node* res = getIntrsctn(h1, h2);
+    if(res) cout << "Intersection at " << res->data << endl;
+    return 0;
+}
